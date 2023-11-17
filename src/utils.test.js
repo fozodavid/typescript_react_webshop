9b8654603f850087a911341a73a4cc0a3ce28308
David Fozo <fozo.david@gmail.com>
2023-11-16 23:17:58 -0500

    Jest setup successful

---
+++ src/utils.test.js
@@ -0,0 +1,8 @@
+// import type * as Example from "../example"
+// const { sillyFunction } = jest.requireActual<typeof Silly>("../silly")
+
+describe("testing test", () => {
+  test("guaranteed random", () => {
+    expect(4).toBe(4)
+  })
+})
